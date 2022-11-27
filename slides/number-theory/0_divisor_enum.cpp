5bad572145866b5b8f8fde2b3da3d343c933bddb
Kyabc <0707.teriyaki@gmail.com>
2022-11-27 00:57:44 +0000

    feat: add divisor enumeration

---
+++ slides/number-theory/0_divisor_enum.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+#include <cassert>
+using namespace std;
+
+vector<int> divisor(int n) {
+    assert(n >= 0);
+    std::vector<int> div;
+    for (long long x = 1; x * x <= n; x++) {
+        if (n % x == 0) {
+            div.push_back(x);
+            div.push_back(n / x);
+        }
+    }
+    return div;
+}
+
+
+#include <iostream>
+
+int main() {
+    int n = 12345;
+    for (int x: divisor(n)) cout << x << ',';
+    //> 1,12345,3,4115,5,2469,15,823
+}
---
+++ slides/number-theory/0_divisor_enum.py
@@ -0,0 +1,17 @@
+def divisor(n: int) -> list[int]:
+    assert(n >= 0)
+    div = []
+    x = 1
+    while x * x <= n:
+        if n % x == 0:
+            div.append(x)
+            div.append(n // x)
+        x += 1
+    return div
+
+
+if __name__ == '__main__':
+    n = 12345
+    for x in divisor(n):
+        print(x)
+    #> 1,12345,3,4115,5,2469,15,823
